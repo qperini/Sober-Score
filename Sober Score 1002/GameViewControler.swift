f62b82e74126aa5b390341b7dbf7f9daefa6ef85
Quinn Perini <qperini@gmail.com>
2018-11-30 02:13:13 -0500

    Go button/timer

--- Sober Score 1002/GameViewControler.swift
+++ Sober Score 1002/GameViewControler.swift
@@ -10,15 +10,27 @@ import UIKit
 
 class GameViewController: UIViewController {
     
-    @IBOutlet weak var NextButton: UIButton!
+    @IBOutlet weak var Next: UIButton!
     @IBOutlet weak var word1: UILabel!
     @IBOutlet weak var word2: UILabel!
     @IBOutlet weak var word3: UILabel!
     @IBOutlet weak var word4: UILabel!
     @IBOutlet weak var word5: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        word1.text = ""
+        word2.text = ""
+        word3.text = ""
+        word4.text = ""
+        word5.text = ""
+        print("Game Loaded")
+    }
+    
+    
+    @IBAction func setWords(_ sender: Any) {
+    (sender as? UIButton)?.isEnabled = false
         let words = ["Horse", "Cat", "Dog", "Fish", "Bird", "Orange", "Yellow", "Blue", "Green", "Black", "Table", "Chair", "Desk", "Bookcase", "Bed", "Teacher", "School", "Student", "Homework", "Class", "Apple", "Banana", "Kiwi", "Grape", "Mango"]
         var testWords = Set<String>()
         
@@ -33,11 +45,16 @@ class GameViewController: UIViewController {
         word3.text = testwordsArray[2]
         word4.text = testwordsArray[3]
         word5.text = testwordsArray[4]
-        print("Game Loaded")
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(30), execute: {
+            self.word1.text = ""
+            self.word2.text = ""
+            self.word3.text = ""
+            self.word4.text = ""
+            self.word5.text = ""
+        })
     }
     
-    @IBAction func NextButton(_ sender: Any) {
-        self.performSegue(withIdentifier: "1to2Arrow", sender: self)
+    @IBAction func Next(_ sender: Any) {
+        self.performSegue(withIdentifier: "1-2Arrow", sender: self)
     }
-    
 }
